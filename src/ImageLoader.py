5f8a81a792e12d2758026ac08aa90c8bfbe9b6cd
Robin Schäfer <r-schaefer@outlook.com>
2022-10-06 18:21:35 +0200

    Replaced with Graphics.py

--- src/ImageLoader.py
+++
@@ -1,5 +0,0 @@
-import pygame
-
-bg = pygame.image.load('../graphics/Background.png')
-bgStars = pygame.image.load('../graphics/Background_stars.png')
-monster = pygame.image.load('../graphics/creature-23.png')
